9e93636241054d756dbec001293cd30a4ee9c2e4
Kokatesrushti <srushtikokate03@gmail.com>
2023-02-02 14:00:54 +0530

    first commit

---
+++ server/models/admin.js
@@ -0,0 +1,54 @@
+const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
+
+const adminSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    lastName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true,
+        trim:true,
+        lowercase:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    cart: {
+        type: [{type:mongoose.Schema.Types.ObjectId,ref:'Product'}] 
+    },
+    hash_pass:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:['user','admin'],
+        default:'user'
+    }
+},{timestamps:true});
+
+userSchema.virtual('password').set(function(password){
+    this.hash_pass=bcrypt.hashSync(password,10)
+});
+
+userSchema.virtual('fullName').get(function(){
+    return `${this.firstName} ${this.lastName}`;
+})
+
+userSchema.methods={
+    authenticate: function(password){
+        return bcrypt.compareSync(password,this.hash_pass)
+    }
+}
+
+module.exports=mongoose.model('Admin',adminSchema);
---
+++ server/models/user.js
@@ -0,0 +1,54 @@
+const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
+
+const userSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    lastName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true,
+        trim:true,
+        lowercase:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    medicinelist:{
+        type: [{type:mongoose.Schema.Types.ObjectId,ref:'Medicine'}] 
+    },
+    hash_pass:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:['user','admin'],
+        default:'user'
+    }
+},{timestamps:true});
+
+userSchema.virtual('password').set(function(password){
+    this.hash_pass=bcrypt.hashSync(password,10)
+});
+
+userSchema.virtual('fullName').get(function(){
+    return `${this.firstName} ${this.lastName}`;
+})
+
+userSchema.methods={
+    authenticate: function(password){
+        return bcrypt.compareSync(password,this.hash_pass)
+    }
+}
+
+module.exports=mongoose.model('User',userSchema);
